2e1df788283c3f2b262fea48731c0aef8e29750d
giannisPlymmenos <giannis.plymmenos@gmail.com>
2023-04-04 13:53:43 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from turtle import Turtle, Screen
+from paddles import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Arcade Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # needs to bounce
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect out of bounds
+    if ball.xcor() > 400:
+        scoreboard.l_point()
+        ball.reset_pos()
+        ball.move()
+
+    if ball.xcor() < -400:
+        scoreboard.r_point()
+        ball.reset_pos()
+        ball.move()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
